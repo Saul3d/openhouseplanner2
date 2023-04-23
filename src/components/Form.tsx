1b8efcd538f9333042419fa594b0ad560b320c16
Saul Solano <knockoutorama@gmail.com>
2023-04-22 22:46:20 -0500

    fixed Dropdown button to have first element selected and styled correctly

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,7 +5,8 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
-const people = [
+const expenseType = [
+  { id: 0, name: 'Select Expense Type' },
   { id: 1, name: 'Indoor Maintenance' },
   { id: 3, name: 'Misc' },
   { id: 2, name: 'Outdoor Maintenance' },
@@ -34,7 +35,7 @@ const Form = () => {
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
-  const [selected, setSelected] = useState(people[0])
+  const [selected, setSelected] = useState(expenseType[0])
   
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-2/3">
@@ -63,10 +64,10 @@ const Form = () => {
       <Listbox value={selected} onChange={setSelected}>
       {({ open }) => (
         <>
-          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Assigned to</Listbox.Label>
+          <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Expense Type</Listbox.Label>
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-              <span className="block truncate">{selected.name}</span>
+              <span className={"block truncate " + (selected.name === expenseType[0].name ? 'text-gray-400' : 'text-gray-900')}>{selected.name}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
               </span>
@@ -80,21 +81,21 @@ const Form = () => {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
+                {expenseType.map((expense) => (
                   <Listbox.Option
-                    key={person.id}
+                    key={expense.id}
                     className={({ active }) =>
                       classNames(
                         active ? 'bg-indigo-600 text-white' : 'text-gray-900',
                         'relative cursor-default select-none py-2 pl-3 pr-9'
                       )
                     }
-                    value={person}
+                    value={expense}
                   >
                     {({ selected, active }) => (
                       <>
                         <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                          {person.name}
+                          {expense.name}
                         </span>
 
                         {selected ? (
